efcafb7200377229468644aeb0e1c871a5874e01
Joaquin Rufino <rufinojoaquin10@gmail.com>
2022-09-16 21:04:20 -0300

    Js

---
+++ code.js
@@ -0,0 +1,36 @@
+//comentario js una sola linea
+/*comentarios en varias lineas*/
+
+//declaracion de variables
+let edad;
+let direccion;
+
+//inicializacion de variables
+edad = 23; //variable numerica
+direccion = "9 de julio"; //tipo cadena de caracteres(string)
+
+//declaracion e inicializacion
+let equipo = "independiente";
+let anioNacimiento = "";
+
+//operaciones matematicas basicas
+let num1 = 6;
+let num2 = 3;
+let suma = num1 + num2; //9
+let resta = num1 - num2; //3
+let multip = num1 * num2; //18
+let division = num1 / num2;//2
+
+//concatenacion
+let dosCadenas = equipo + direccion;//si lo visualizo quedaria independiente y la direccion todo junto
+let cadenaYNum = equipo + edad; //quedaria independiente23
+
+//prompt - ingreso de datos
+let usuario = prompt ("Ingresa tu nombre de usuario");
+let email = prompt ("ingresa tu email");
+
+//alert - salida de datos 
+alert ("Buenos dias " +usuario);
+
+
+
