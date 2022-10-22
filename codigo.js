736dbd9d710bc570344d5d309f7f5bf9e2d518c3
Joaquin Rufino <rufinojoaquin10@gmail.com>
2022-10-22 10:35:46 -0300

    "Js"

--- codigo.js
+++ codigo.js
@@ -2,56 +2,42 @@
 //esto funcionara con un solo producto, si utiizas mas habra algunas variantes
 const carrito = [];
 let totalCarrito;
-const fotoLibro = {
-    imagen: "../assets/img/foto-3.jpg",
-    nombre: "Descubre y dejate descubrir",
-    publicacion: "Publicado en 2019",
-    precio:2500
-}
+let contenedor= document.getElementById("tarjetas");
 
-let tarjetas = document.getElementById("tarjetas");
-tarjetas.innerHTML = `
-    <div class="card" style="width: 26rem;" >
-        <img src=${fotoLibro.imagen} class="card-img-top" alt=${fotoLibro.nombre}>
+function renderizarTarjetas(){
+    for(const libro of fotoLibro){
+        contenedor.innerHTML += `
+        <div class="card" style="width: 26rem;" >
+        <img src=${libro.imagen} class="card-img-top" alt=${libro.nombre}>
         <div class="card-body">
-            <h5 class="card-title">${fotoLibro.nombre}</h5>
-            <p class="card-text">$ ${fotoLibro.precio}</p>
-            <button id="mybutton" class="btn btn-primary">Comprar</button>
+        <h5 class="card-title">${libro.nombre}</h5>
+        <p class="card-text">$ ${libro.precio}</p>
+        <button id="btn${libro.id}" class="btn btn-primary">Comprar</button>
         </div>
-    </div>
-`;
-
-const boton = document.getElementById("mybutton");
-
-//opcion 1
-
-boton.addEventListener("click",agregarAlCarro);
-
-function agregarAlCarro(){
-    //aqui puedo agregar una variable sumadora
-    //para poder con for ir sumando i++ y que cuenten los prodcutos
-    alert("Agregaste un nuevo producto al carrito");
-    carrito.push(fotoLibro);
-    console.table(carrito);
+        </div>
+        `;
+    }
+    //eventos
+    fotoLibro.forEach(libro => {
+    //evento para cada boton que pondre
+    document.getElementById(`btn${libro.id}`).addEventListener("click",function (){
+        agregarAlCarrito(libro);
+    });
+    })
 }
 
-//podria ser un globito abajo del boton con algun mensaje
+renderizarTarjetas();
+
+/*
 boton.onmouseover = () => {
     //de esta forma le cambiamos el color al boton cuando pasamos por arriba
     boton.className="btn btn-success";
 }
-
 boton.onmouseout = () => {
     //de esta forma le devolvemos el color original al boton cuando salimos de arriba con el mouse
     boton.className="btn btn-primary";
 }
-
-
-
-        //evento para cada boton(ponemos una funcion para que reciba parametros porque el addeventlistener no me permite recibir parametros)
-        document.getElementById(`${mybutton}`).addEventListener("click",function(){
-            agregarAlCarrito(libros);
-        });
+*/
 
 function agregarAlCarrito(libroComprado){
     carrito.push(libroComprado);
@@ -59,12 +45,11 @@ function agregarAlCarrito(libroComprado){
     alert("Producto: "+libroComprado.nombre+" agregado al carrito!!");
     document.getElementById("tablabody").innerHTML += `
         <tr>
-            <td>${libroComprado.publicaciom}</td>
+            <td>${libroComprado.id}</td>
             <td>${libroComprado.nombre}</td>
             <td>${libroComprado.precio}</td>
         </tr>
     `;
-    totalCarrito = carrito.reduce((acumulador,libros)=> acumulador + libros.precio, 0);
-    document.getElementById("total").innerHTML = "Total a pagar: $"+totalCarrito;
-
+    totalCarrito = carrito.reduce((acumulador,libro)=> acumulador + libro.precio, 0);
+    document.getElementById("total").innerText = "Total a pagar: $"+totalCarrito;
 }
---
+++ productos.js
@@ -0,0 +1,8 @@
+const fotoLibro = [{
+    id:1,
+    imagen: "../assets/img/foto-3.jpg",
+    nombre: "Descubre y dejate descubrir",
+    publicacion: "Publicado en 2019",
+    precio:2500
+}
+];
