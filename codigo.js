fd9f29de7f88b8749a9ca7ffcae66c36abb052c5
Joaquin Rufino <rufinojoaquin10@gmail.com>
2022-10-23 09:47:38 -0300

    "Dark/light mode"

--- codigo.js
+++ codigo.js
@@ -24,20 +24,20 @@ function renderizarTarjetas(){
         agregarAlCarrito(libro);
     });
     })
+    
 }
-
-renderizarTarjetas();
-
-
-/*
-boton.onmouseover = () => {
-    //de esta forma le cambiamos el color al boton cuando pasamos por arriba
-    boton.className="btn btn-success";
-}
-boton.onmouseout = () => {
-    //de esta forma le devolvemos el color original al boton cuando salimos de arriba con el mouse
-    boton.className="btn btn-primary";
-}
+    
+    renderizarTarjetas();
+    
+    /*
+    boton.onmouseover = () => {
+        //de esta forma le cambiamos el color al boton cuando pasamos por arriba
+        boton.className="btn btn-success";
+    }
+    boton.onmouseout = () => {
+        //de esta forma le devolvemos el color original al boton cuando salimos de arriba con el mouse
+        boton.className="btn btn-primary";
+    }
 */
 
 function agregarAlCarrito(libroComprado){
@@ -54,4 +54,47 @@ function agregarAlCarrito(libroComprado){
     totalCarrito = carrito.reduce((acumulador,libro)=> acumulador + libro.precio, 0);
     document.getElementById("total").innerText = "Total a pagar: $"+totalCarrito;
 }
+
+
+// dark/light mode
+const boton = document.getElementById("mode");
+const cuerpo = document.getElementById("cuerpo")
+const principal = document.getElementById("principal");
+//de esta forma quedara guardado el modo que indiquemos cuando volvamos a ingresar
+let modo = localStorage.getItem("modo");
+
+//primer renderizado, con esto cuando ingreses esta el modo que indicaste
+if(modo !=null){
+    document.body.className=modo;
+    cuerpo.className="pt-0 mt-0 mb-0 m-0 p-0 "+modo;
+    if(modo=="dark"){
+        boton.innerText="Light mode";
+    }else{
+        boton.innerText="Dark mode";
+    }
+}else{
+    modo="dark";
+}
+
+boton.onclick = () => {
+    if(modo=="dark"){
+        document.body.className="light";
+        //de esta forma con classlist seleccionamos toda la lista de clase
+        //y removemos lo que queremos de esa lista para no nombrar todas las clases
+        //luego agregamos la que queremos
+        principal.classList.remove("dark");
+        principal.classList.add("light");
+        boton.innerText="Dark mode";
+        modo="light";
+    }else{
+        document.body.className="dark";
+        principal.classList.remove("light");
+        principal.classList.add("dark");
+        boton.innerText="Light mode";
+        modo="dark";
+    }
+    localStorage.setItem("modo", modo);
+}
+
+
 
